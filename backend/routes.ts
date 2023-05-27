85b49c2c246eefbaeb1a8104f3913a5031fc81da
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-27 15:56:55 +0200

    Setting up uuid and server test

--- backend/routes.ts
+++ backend/routes.ts
@@ -1,8 +1,17 @@
 import express from 'express';
-
+import { v4 as uuidv4 } from 'uuid';
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('App is responding');
+});
+
+app.post('/api/game', (req, res) => {
+    
+
+    res.status(201).json(gameId);
 })
+
 export default app;
