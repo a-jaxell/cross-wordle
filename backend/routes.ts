b045bd2f3f4b098c44a621bef60db3856df01d4a
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-05-27 17:27:28 +0200

    Game ID is created and returned

--- backend/routes.ts
+++ backend/routes.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import findWord from './util/findWord.js';
+import Ordel from './util/matchWord.js';
+import words from './words/words.js';
+
 const app = express();
 
 app.use(express.json());
@@ -10,6 +14,20 @@ app.get('/', (req, res) => {
 
 app.post('/api/game', (req, res) => {
     
+    const {length, multi } = req.body;
+    const newWord = findWord(words, length, multi);
+
+    const gameId = {
+        id: uuidv4(),
+        multi: multi,
+        length: length,
+        correctWord: newWord,
+        guesses: [],
+        startTime: new Date(),
+        endTime: null,
+    }
+
+    // Push to database and then respond with correctWord omitted.
 
     res.status(201).json(gameId);
 })
