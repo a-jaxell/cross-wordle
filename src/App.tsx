0d06b0019ff2b8eafc5395b646a5fc0a0d33b0df
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-06-05 11:48:06 +0200

    Add fetch of game datafrom api

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,25 @@ import StartGame from "./components/StartGame";
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState();
+  const [gameId, setGameId] = useState();
 
   return (
     <>
       {/* <Header /> */}
-      <StartGame onStartGame={(length, multi)=> {
-        console.log(length, multi);
+      <StartGame onStartGame={ async (length, multi)=> {
+        const res = await fetch('/api/game', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            length,
+            multi
+          })
+        })
+
+        const data = await res.json()
+        setGameId(data.id);
       }} />
       {/* <Game matchResponse={mockMatch} /> */}
     </>
