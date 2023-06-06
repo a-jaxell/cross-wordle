c33e860f12103e069a2beba7f316038951d019e9
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-06-06 17:18:46 +0200

    fix .env errors

--- backend/database/databaseConnect.ts
+++ backend/database/databaseConnect.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
+dotenv.config();
 export default async function databaseConnect(){
    await mongoose.connect(process.env.MONGODB_URI as string).catch((error)=> {
         console.log(error);
