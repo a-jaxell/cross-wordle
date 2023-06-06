23e9f0402141391d4bac997237332b74eea97105
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-06-06 18:12:58 +0200

    Add styling

--- src/components/DisplayHighscore/index.tsx
+++ src/components/DisplayHighscore/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { HighscoreObject } from "../../App";
-import { DateTime, Interval } from "luxon";
+import { DateTime } from "luxon";
+import styles from './style.module.scss'
 
 type DisplayHighscoreProps = {
   highscore: HighscoreObject;
@@ -10,10 +11,9 @@ const DisplayHighscore: FC<DisplayHighscoreProps> = ({ highscore }) => {
   const start = DateTime.fromISO(highscore.startTime);
   const end = DateTime.fromISO(highscore.endTime);
   const time = end.diff(start, ["minutes", "seconds"]).toFormat("mm:ss");
-  console.log(time)
 
   return (
-    <div>
+    <div className={styles.formContainer}>
       <h4>{`Well done ${highscore.name}!`}</h4>
       <p>{`It took you ${highscore.guesses.length} guesses and ${time} to guess
       ${highscore.correctWord} correctly`}</p>
